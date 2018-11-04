55270531d2a1143f893dc43c7a7d4506e301c0f3
Yucked <vuxdev@gmail.com>
2018-11-04 10:49:41 -0800

    Removed useless classes.

--- Objects/TrackExceptionData.cs
+++
@@ -1,9 +0,0 @@
-namespace Victoria.Objects
-{
-    internal struct TrackExceptionData
-    {
-        public string Error { get; internal set; }
-        public LavaPlayer LavaPlayer { get; internal set; }
-        public LavaTrack Track { get; internal set; }
-    }
-}
