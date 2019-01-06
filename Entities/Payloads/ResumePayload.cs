3e45a748743f0cbc5984dcd290cf952d69082824
Yucked <vuxdev@gmail.com>
2019-01-05 17:17:12 -0800

    ResumePayload for websocket resume

---
+++ Entities/Payloads/ResumePayload.cs
@@ -0,0 +1,20 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Victoria.Entities.Payloads
+{
+    internal sealed class ResumePayload : LavaPayload
+    {
+        [JsonProperty("key")]
+        public string Key { get; }
+
+        [JsonProperty("timeout")]
+        public int Timeout { get; }
+        
+        public ResumePayload(string key, TimeSpan timeSpan) : base("configureResuming")
+        {
+            Key = key;
+            Timeout = timeSpan.Seconds;
+        }        
+    }
+}
