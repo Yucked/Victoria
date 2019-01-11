6633e5a84f4a20c6c9598bf0add09323d13f7ab2
Yucked <vuxdev@gmail.com>
2019-01-11 12:59:27 -0800

    Single play payload. Added noReplace.

--- Entities/Payloads/PlayPayload.cs
+++ Entities/Payloads/PlayPayload.cs
@@ -1,3 +1,4 @@
+using System;
 using Newtonsoft.Json;
 
 namespace Victoria.Entities.Payloads
@@ -7,9 +8,21 @@ namespace Victoria.Entities.Payloads
         [JsonProperty("track")] 
         public string Track { get; }
 
-        public PlayPayload(string trackString, ulong guildId) : base("play", guildId)
+        [JsonProperty("startTime")] 
+        public long StartTime { get; }
+
+        [JsonProperty("stopTime")] 
+        public long StopTime { get; }
+        
+        [JsonProperty("noReplace")]
+        public bool ShouldReplace { get; }
+
+        public PlayPayload(string trackString, TimeSpan start, TimeSpan stop, bool shouldReplace, ulong guildId) : base("play", guildId)
         {
             Track = trackString;
+            StartTime = (long) start.TotalMilliseconds;
+            StopTime = (long) stop.TotalMilliseconds;
+            ShouldReplace = shouldReplace;
         }
     }
 }
