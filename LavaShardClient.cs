74574d61a688e1fa92589eb277ed9e45a0917c28
Lewis Crichton <lewistehminerz@outlook.com>
2019-04-17 14:51:12 +0100

    fix when configuration param isn't passed
    default == null

--- LavaShardClient.cs
+++ LavaShardClient.cs
@@ -15,8 +15,9 @@ namespace Victoria
         /// </summary>
         /// <param name="socketClient"><see cref="DiscordSocketClient"/></param>
         /// <param name="configuration"><see cref="Configuration"/></param>
-        public Task StartAsync(DiscordShardedClient shardedClient, Configuration configuration = default)
+        public Task StartAsync(DiscordShardedClient shardedClient, Configuration configuration = null)
         {
+            if (configuration == null) configuration = new Configuration();
             shardedClient.ShardDisconnected += OnShardDisconnected;
             return InitializeAsync(shardedClient, configuration);
         }
