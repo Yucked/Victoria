cd109c46d02428c800a6b45be809bc0c527bbd23
Yucked <yucked@protonmail.com>
2020-04-22 19:21:56 -0700

    Need to write converter for RouteStatus

---
+++ src/Converters/RouteResponseConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Victoria.Responses.Rest;
+
+namespace Victoria.Converters {
+	internal sealed class RouteResponseConverter : JsonConverter<RouteStatus> {
+		public override RouteStatus Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+			throw new NotImplementedException();
+		}
+
+		public override void Write(Utf8JsonWriter writer, RouteStatus value, JsonSerializerOptions options) {
+			throw new NotImplementedException();
+		}
+	}
+}
