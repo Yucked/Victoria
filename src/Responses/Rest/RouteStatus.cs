72ba07c1709ad8b55e911367ae92ecf26d4953df
Yucked <yucked@protonmail.com>
2020-04-22 19:20:45 -0700

    RoutePlanner models

---
+++ src/Responses/Rest/RouteStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace Victoria.Responses.Rest {
+	/// <summary>
+	/// Contains information about route planner class and details.
+	/// </summary>
+	public struct RouteStatus {
+		/// <summary>
+		/// Which planner class is being used.
+		/// </summary>
+		public string Class { get; internal set; }
+
+		/// <summary>
+		/// Gives more information about route planner.
+		/// </summary>
+		public RouteDetail Details { get; internal set; }
+	}
+}
