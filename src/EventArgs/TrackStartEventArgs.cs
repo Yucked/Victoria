f2821281e75bf022218d2821e3939b88f64b3679
Yucked <mail@dvlp.nill.sh>
2020-04-22 18:57:14 -0700

    Added support for TrackFinished event

---
+++ src/EventArgs/TrackStartEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using Victoria.Decoder;
+using Victoria.Responses.WebSocket;
+
+namespace Victoria.EventArgs {
+	/// <summary>
+	/// Information about the track that started.
+	/// </summary>
+	public readonly struct TrackStartEventArgs {
+		/// <summary>
+		///     Player for which this event fired.
+		/// </summary>
+		public LavaPlayer Player { get; }
+
+		/// <summary>
+		///     Track sent by Lavalink.
+		/// </summary>
+		public LavaTrack Track { get; }
+
+		internal TrackStartEventArgs(LavaPlayer player, TrackStartEvent trackStartEvent) {
+			Player = player;
+			Track = TrackDecoder.Decode(trackStartEvent.Hash);
+		}
+	}
+}
