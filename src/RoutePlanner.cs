08bec9f08397cbf34d0ef377d24047ed52a841db
Yucked <mail@dvlp.nill.sh>
2020-04-22 19:21:38 -0700

    Completed all methods

--- src/RoutePlanner.cs
+++ src/RoutePlanner.cs
@@ -3,72 +3,87 @@ using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Victoria.Converters;
 using Victoria.Responses.Rest;
 
 namespace Victoria {
-    /// <summary>
-    /// 
-    /// </summary>
-    public sealed class RoutePlanner {
-        private readonly HttpClient _httpClient;
+	/// <summary>
+	/// Interacting with Lavalink's RoutePlanner API.
+	/// </summary>
+	public readonly struct RoutePlanner {
+		private readonly HttpClient _httpClient;
 
-        internal RoutePlanner(HttpClient httpClient) {
-            _httpClient = httpClient;
-        }
+		private static readonly JsonSerializerOptions SerializerOptions
+			= new JsonSerializerOptions {
+				Converters = {
+					new RouteResponseConverter()
+				}
+			};
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public Task GetStatusAsync() {
-            throw new NotImplementedException("Waiting on Lavalink docs to get fixed.");
-        }
+		internal RoutePlanner(HttpClient httpClient) {
+			_httpClient = httpClient;
+		}
 
-        /// <summary>
-        /// Unmark a failed address.
-        /// </summary>
-        /// <param name="address"></param>
-        /// <returns></returns>
-        /// <exception cref="Exception">Throws if Lavalink throws an exception.</exception>
-        public async Task FreeAddressAsync(string address) {
-            var payload = JsonSerializer.SerializeToUtf8Bytes(new {
-                address
-            });
+		/// <summary>
+		/// Returns the current status of route planner.
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="NotImplementedException"></exception>
+		public async Task<RouteStatus> GetStatusAsync() {
+			using var requestMessage = new HttpRequestMessage(HttpMethod.Get, "/routeplanner/status");
+			using var responseMessage = await _httpClient.SendAsync(requestMessage)
+			   .ConfigureAwait(false);
 
-            var responseMessage = await _httpClient
-                .PostAsync("/routeplanner/free/address", new ByteArrayContent(payload))
-                .ConfigureAwait(false);
+			var responseContent = await responseMessage.Content.ReadAsByteArrayAsync()
+			   .ConfigureAwait(false);
 
-            if (responseMessage.IsSuccessStatusCode) {
-                return;
-            }
+			return JsonSerializer.Deserialize<RouteStatus>(responseContent, SerializerOptions);
+		}
 
-            var responseContent = await responseMessage.Content.ReadAsByteArrayAsync()
-                .ConfigureAwait(false);
+		/// <summary>
+		/// Unmark a failed address.
+		/// </summary>
+		/// <param name="address"></param>
+		/// <returns></returns>
+		/// <exception cref="Exception">Throws if Lavalink throws an exception.</exception>
+		public async Task FreeAddressAsync(string address) {
+			var payload = JsonSerializer.SerializeToUtf8Bytes(new {
+				address
+			});
 
-            var routeResponse = JsonSerializer.Deserialize<RouteResponse>(responseContent);
-            throw new Exception($"{routeResponse.Error} - {routeResponse.Message}");
-        }
+			var responseMessage = await _httpClient
+			   .PostAsync("/routeplanner/free/address", new ByteArrayContent(payload))
+			   .ConfigureAwait(false);
 
-        /// <summary>
-        /// Unmark all failed address.
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="Exception">Throws if Lavalink throws an exception.</exception>
-        public async Task FreeAllAsync() {
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/routeplanner/free/all");
-            using var responseMessage = await _httpClient.SendAsync(requestMessage)
-                .ConfigureAwait(false);
+			if (responseMessage.IsSuccessStatusCode) {
+				return;
+			}
 
-            if (responseMessage.StatusCode == HttpStatusCode.NoContent)
-                return;
+			var responseContent = await responseMessage.Content.ReadAsByteArrayAsync()
+			   .ConfigureAwait(false);
 
-            var responseContent = await responseMessage.Content.ReadAsByteArrayAsync()
-                .ConfigureAwait(false);
+			var routeResponse = JsonSerializer.Deserialize<RouteResponse>(responseContent);
+			throw new Exception($"{routeResponse.Error} - {routeResponse.Message}");
+		}
 
-            var routeResponse = JsonSerializer.Deserialize<RouteResponse>(responseContent);
-            throw new Exception($"{routeResponse.Error} - {routeResponse.Message}");
-        }
-    }
+		/// <summary>
+		/// Unmark all failed address.
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="Exception">Throws if Lavalink throws an exception.</exception>
+		public async Task FreeAllAsync() {
+			using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/routeplanner/free/all");
+			using var responseMessage = await _httpClient.SendAsync(requestMessage)
+			   .ConfigureAwait(false);
+
+			if (responseMessage.StatusCode == HttpStatusCode.NoContent)
+				return;
+
+			var responseContent = await responseMessage.Content.ReadAsByteArrayAsync()
+			   .ConfigureAwait(false);
+
+			var routeResponse = JsonSerializer.Deserialize<RouteResponse>(responseContent);
+			throw new Exception($"{routeResponse.Error} - {routeResponse.Message}");
+		}
+	}
 }
