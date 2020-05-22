324bde119af6c9882b7e92f88125565bda0fed90
Yucked <yucked@protonmail.com>
2020-05-22 19:57:58 -0400

    Also fixed equalizer

--- src/LavaPlayer.cs
+++ src/LavaPlayer.cs
@@ -8,77 +8,77 @@ using Victoria.Interfaces;
 using Victoria.Payloads;
 
 namespace Victoria {
-    /// <summary>
-    /// </summary>
-    public class LavaPlayer : IAsyncDisposable {
-        /// <summary>
-        /// </summary>
-        public IReadOnlyCollection<EqualizerBand> Equalizer
-			=> _equalizer;
+	/// <summary>
+	/// </summary>
+	public class LavaPlayer : IAsyncDisposable {
+		/// <summary>
+		/// </summary>
+		public IReadOnlyCollection<EqualizerBand> Equalizer
+			=> _equalizer.Values as IReadOnlyCollection<EqualizerBand>;
 
-        /// <summary>
-        ///     Last time player was updated.
-        /// </summary>
-        public DateTimeOffset LastUpdate { get; internal set; }
+		/// <summary>
+		///     Last time player was updated.
+		/// </summary>
+		public DateTimeOffset LastUpdate { get; internal set; }
 
-        /// <summary>
-        ///     Player's current state.
-        /// </summary>
-        public PlayerState PlayerState { get; internal set; }
+		/// <summary>
+		///     Player's current state.
+		/// </summary>
+		public PlayerState PlayerState { get; internal set; }
 
-        /// <summary>
-        ///     Default queue.
-        /// </summary>
-        public DefaultQueue<IQueueable> Queue { get; private set; }
+		/// <summary>
+		///     Default queue.
+		/// </summary>
+		public DefaultQueue<IQueueable> Queue { get; private set; }
 
-        /// <summary>
-        ///     Channel bound to this player.
-        /// </summary>
-        public ITextChannel TextChannel { get; internal set; }
+		/// <summary>
+		///     Channel bound to this player.
+		/// </summary>
+		public ITextChannel TextChannel { get; internal set; }
 
-        /// <summary>
-        ///     Current track that is playing.
-        /// </summary>
-        public LavaTrack Track { get; internal set; }
+		/// <summary>
+		///     Current track that is playing.
+		/// </summary>
+		public LavaTrack Track { get; internal set; }
 
-        /// <summary>
-        ///     Voice channel this player is connected to.
-        /// </summary>
-        public IVoiceChannel VoiceChannel { get; internal set; }
+		/// <summary>
+		///     Voice channel this player is connected to.
+		/// </summary>
+		public IVoiceChannel VoiceChannel { get; internal set; }
 
-        /// <summary>
-        ///     Voice server this player is connected to.
-        /// </summary>
-        public SocketVoiceServer VoiceServer { get; internal set; }
+		/// <summary>
+		///     Voice server this player is connected to.
+		/// </summary>
+		public SocketVoiceServer VoiceServer { get; internal set; }
 
-        /// <summary>
-        ///     Player's current voice state.
-        /// </summary>
-        public IVoiceState VoiceState { get; internal set; }
+		/// <summary>
+		///     Player's current voice state.
+		/// </summary>
+		public IVoiceState VoiceState { get; internal set; }
 
-        /// <summary>
-        ///     Player's current volume.
-        /// </summary>
-        public int Volume { get; private set; }
+		/// <summary>
+		///     Player's current volume.
+		/// </summary>
+		public int Volume { get; private set; }
 
-		private readonly List<EqualizerBand> _equalizer;
+		private readonly IDictionary<int, EqualizerBand> _equalizer;
 
 		private readonly LavaSocket _lavaSocket;
 
-        /// <summary>
-        ///     Represents a <see cref="IGuild" /> voice connection.
-        /// </summary>
-        /// <param name="lavaSocket">
-        ///     <see cref="LavaSocket" />
-        /// </param>
-        /// <param name="voiceChannel">Voice channel to connect to.</param>
-        /// <param name="textChannel">Text channel this player is bound to.</param>
-        public LavaPlayer(LavaSocket lavaSocket, IVoiceChannel voiceChannel, ITextChannel textChannel) {
+		/// <summary>
+		///     Represents a <see cref="IGuild" /> voice connection.
+		/// </summary>
+		/// <param name="lavaSocket">
+		///     <see cref="LavaSocket" />
+		/// </param>
+		/// <param name="voiceChannel">Voice channel to connect to.</param>
+		/// <param name="textChannel">Text channel this player is bound to.</param>
+		public LavaPlayer(LavaSocket lavaSocket, IVoiceChannel voiceChannel, ITextChannel textChannel) {
 			_lavaSocket = lavaSocket;
 			VoiceChannel = voiceChannel;
 			TextChannel = textChannel;
 			Queue = new DefaultQueue<IQueueable>(69);
-			_equalizer = new List<EqualizerBand>(15);
+			_equalizer = new Dictionary<int, EqualizerBand>(15);
 		}
 
 		/// <inheritdoc />
@@ -99,11 +99,11 @@ namespace Victoria {
 			PlayerState = PlayerState.Disconnected;
 		}
 
-        /// <summary>
-        ///     Plays the specified track.
-        /// </summary>
-        /// <param name="track">An instance of <see cref="LavaTrack" />.</param>
-        public async Task PlayAsync(LavaTrack track) {
+		/// <summary>
+		///     Plays the specified track.
+		/// </summary>
+		/// <param name="track">An instance of <see cref="LavaTrack" />.</param>
+		public async Task PlayAsync(LavaTrack track) {
 			if (track == null) {
 				throw new ArgumentNullException(nameof(track));
 			}
@@ -116,16 +116,16 @@ namespace Victoria {
 			PlayerState = PlayerState.Playing;
 		}
 
-        /// <summary>
-        ///     Plays the specified track with a custom start and end time.
-        /// </summary>
-        /// <param name="track">An instance of <see cref="LavaTrack" />.</param>
-        /// <param name="startTime">Custom start time for track. Must be greater than 0.</param>
-        /// <param name="endTime">Custom end time for track. Must be less than <see cref="LavaTrack.Duration" />.</param>
-        /// <param name="noReplace">If true, this operation will be ignored if a track is already playing or paused.</param>
-        /// <exception cref="ArgumentOutOfRangeException">Throws when start or end time are out of range.</exception>
-        /// <exception cref="InvalidOperationException">Throws when star time is bigger than end time.</exception>
-        public async Task PlayAsync(LavaTrack track, TimeSpan startTime, TimeSpan endTime, bool noReplace = false) {
+		/// <summary>
+		///     Plays the specified track with a custom start and end time.
+		/// </summary>
+		/// <param name="track">An instance of <see cref="LavaTrack" />.</param>
+		/// <param name="startTime">Custom start time for track. Must be greater than 0.</param>
+		/// <param name="endTime">Custom end time for track. Must be less than <see cref="LavaTrack.Duration" />.</param>
+		/// <param name="noReplace">If true, this operation will be ignored if a track is already playing or paused.</param>
+		/// <exception cref="ArgumentOutOfRangeException">Throws when start or end time are out of range.</exception>
+		/// <exception cref="InvalidOperationException">Throws when star time is bigger than end time.</exception>
+		public async Task PlayAsync(LavaTrack track, TimeSpan startTime, TimeSpan endTime, bool noReplace = false) {
 			if (track == null) {
 				throw new ArgumentNullException(nameof(track));
 			}
@@ -150,10 +150,10 @@ namespace Victoria {
 			PlayerState = PlayerState.Playing;
 		}
 
-        /// <summary>
-        ///     Stops the current track if any is playing.
-        /// </summary>
-        public async Task StopAsync() {
+		/// <summary>
+		///     Stops the current track if any is playing.
+		/// </summary>
+		public async Task StopAsync() {
 			var payload = new StopPayload(VoiceChannel.GuildId);
 			await _lavaSocket.SendAsync(payload)
 			   .ConfigureAwait(false);
@@ -161,10 +161,10 @@ namespace Victoria {
 			PlayerState = PlayerState.Stopped;
 		}
 
-        /// <summary>
-        ///     Pauses the current track if any is playing.
-        /// </summary>
-        public async Task PauseAsync() {
+		/// <summary>
+		///     Pauses the current track if any is playing.
+		/// </summary>
+		public async Task PauseAsync() {
 			if (!PlayerState.EnsureState()) {
 				throw new InvalidOperationException(
 					"Player state doesn't match any of the following states: Connected, Playing, Paused.");
@@ -179,10 +179,10 @@ namespace Victoria {
 				: PlayerState.Paused;
 		}
 
-        /// <summary>
-        ///     Resumes the current track if any is playing.
-        /// </summary>
-        public async Task ResumeAsync() {
+		/// <summary>
+		///     Resumes the current track if any is playing.
+		/// </summary>
+		public async Task ResumeAsync() {
 			if (!PlayerState.EnsureState()) {
 				throw new InvalidOperationException(
 					"Player state doesn't match any of the following states: Connected, Playing, Paused.");
@@ -197,14 +197,14 @@ namespace Victoria {
 				: PlayerState.Playing;
 		}
 
-        /// <summary>
-        ///     Skips the current track after the specified delay.
-        /// </summary>
-        /// <param name="delay">If set to null, skips instantly otherwise after the specified value.</param>
-        /// <returns>
-        ///     The next <see cref="LavaTrack" />.
-        /// </returns>
-        public async Task<LavaTrack> SkipAsync(TimeSpan? delay = default) {
+		/// <summary>
+		///     Skips the current track after the specified delay.
+		/// </summary>
+		/// <param name="delay">If set to null, skips instantly otherwise after the specified value.</param>
+		/// <returns>
+		///     The next <see cref="LavaTrack" />.
+		/// </returns>
+		public async Task<LavaTrack> SkipAsync(TimeSpan? delay = default) {
 			if (!PlayerState.EnsureState()) {
 				throw new InvalidOperationException(
 					"Player state doesn't match any of the following states: Connected, Playing, Paused.");
@@ -225,12 +225,12 @@ namespace Victoria {
 			return track;
 		}
 
-        /// <summary>
-        ///     Seeks the current track to specified position.
-        /// </summary>
-        /// <param name="position">Position must be less than <see cref="LavaTrack.Duration" />.</param>
-        /// <returns></returns>
-        public async Task SeekAsync(TimeSpan? position) {
+		/// <summary>
+		///     Seeks the current track to specified position.
+		/// </summary>
+		/// <param name="position">Position must be less than <see cref="LavaTrack.Duration" />.</param>
+		/// <returns></returns>
+		public async Task SeekAsync(TimeSpan? position) {
 			if (position == null) {
 				throw new ArgumentNullException(nameof(position));
 			}
@@ -250,23 +250,23 @@ namespace Victoria {
 			   .ConfigureAwait(false);
 		}
 
-        /// <summary>
-        ///     Changes the current volume and updates <see cref="Volume" />.
-        /// </summary>
-        public async Task UpdateVolumeAsync(ushort volume) {
+		/// <summary>
+		///     Changes the current volume and updates <see cref="Volume" />.
+		/// </summary>
+		public async Task UpdateVolumeAsync(ushort volume) {
 			Volume = volume;
 			var payload = new VolumePayload(VoiceChannel.GuildId, volume);
 			await _lavaSocket.SendAsync(payload)
 			   .ConfigureAwait(false);
 		}
 
-        /// <summary>
-        ///     Change the <see cref="LavaPlayer" />'s equalizer. There are 15 bands (0-14) that can be changed.
-        /// </summary>
-        /// <param name="bands">
-        ///     <see cref="EqualizerBand" />
-        /// </param>
-        public async Task EqualizerAsync(params EqualizerBand[] bands) {
+		/// <summary>
+		///     Change the <see cref="LavaPlayer" />'s equalizer. There are 15 bands (0-14) that can be changed.
+		/// </summary>
+		/// <param name="bands">
+		///     <see cref="EqualizerBand" />
+		/// </param>
+		public async Task EqualizerAsync(params EqualizerBand[] bands) {
 			if (!PlayerState.EnsureState()) {
 				throw new InvalidOperationException(
 					"Player state doesn't match any of the following states: Connected, Playing, Paused.");
