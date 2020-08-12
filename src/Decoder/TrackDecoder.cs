0b63de9013d87673cdd50f6697169ccb7621837a
Yucked <yucked@protonmail.com>
2020-08-11 22:00:22 -0400

    Reflecting changes regarding LavaTrack ctor

--- src/Decoder/TrackDecoder.cs
+++ src/Decoder/TrackDecoder.cs
@@ -3,43 +3,45 @@ using System.Buffers.Text;
 using System.Text;
 
 namespace Victoria.Decoder {
-	/// <summary>
-	/// Helper class for decoding Lavalink's <see cref="LavaTrack"/> hash.
-	/// </summary>
-	public readonly struct TrackDecoder {
-		/// <summary>
-		///     Decodes the hash for the specified track.
-		/// </summary>
-		/// <param name="hash">Track's hash.</param>
-		/// <returns>
-		///     <see cref="LavaTrack" />
-		/// </returns>
-		public static LavaTrack Decode(string hash) {
-			Span<byte> hashBuffer = stackalloc byte[hash.Length];
-			Encoding.ASCII.GetBytes(hash, hashBuffer);
-			Base64.DecodeFromUtf8InPlace(hashBuffer, out var bytesWritten);
-			var javaReader = new JavaBinaryReader(hashBuffer.Slice(0, bytesWritten));
+    /// <summary>
+    /// Helper class for decoding Lavalink's <see cref="LavaTrack"/> hash.
+    /// </summary>
+    public readonly struct TrackDecoder {
+        /// <summary>
+        ///     Decodes the hash for the specified track.
+        /// </summary>
+        /// <param name="hash">Track's hash.</param>
+        /// <returns>
+        ///     <see cref="LavaTrack" />
+        /// </returns>
+        public static LavaTrack Decode(string hash) {
+            Span<byte> hashBuffer = stackalloc byte[hash.Length];
+            Encoding.ASCII.GetBytes(hash, hashBuffer);
+            Base64.DecodeFromUtf8InPlace(hashBuffer, out var bytesWritten);
+            var javaReader = new JavaBinaryReader(hashBuffer.Slice(0, bytesWritten));
 
-			// Reading header
-			var header = javaReader.Read<int>();
-			var flags = (int) ((header & 0xC0000000L) >> 30);
-			var hasVersion = (flags & 1) != 0;
-			var version = hasVersion
-				? javaReader.Read<sbyte>()
-				: 1;
+            // Reading header
+            var header = javaReader.Read<int>();
+            var flags = (int) ((header & 0xC0000000L) >> 30);
+            var hasVersion = (flags & 1) != 0;
+            var version = hasVersion
+                ? javaReader.Read<sbyte>()
+                : 1;
 
-			var track = new LavaTrack()
-			   .WithHash(hash)
-			   .WithTitle(javaReader.ReadString())
-			   .WithAuthor(javaReader.ReadString())
-			   .WithDuration(javaReader.Read<long>())
-			   .WithId(javaReader.ReadString())
-			   .WithStream(javaReader.Read<bool>())
-			   .WithUrl(javaReader.Read<bool>()
-					? javaReader.ReadString()
-					: string.Empty);
+            var track = new LavaTrack(
+                hash,
+                title: javaReader.ReadString(),
+                author: javaReader.ReadString(),
+                duration: javaReader.Read<long>(),
+                id: javaReader.ReadString(),
+                isStream: javaReader.Read<bool>(),
+                url: javaReader.Read<bool>()
+                    ? javaReader.ReadString()
+                    : string.Empty,
+                position: default,
+                canSeek: true);
 
-			return track;
-		}
-	}
+            return track;
+        }
+    }
 }
