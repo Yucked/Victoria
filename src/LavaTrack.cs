320eb6c85a0045065416369fd86ad38a90b188b9
yucked <yucked@protonmail.com>
2020-09-10 18:59:50 -0400

    Opened up LavaTrack constructor

--- src/LavaTrack.cs
+++ src/LavaTrack.cs
@@ -1,55 +1,54 @@
 using System;
-using Victoria.Interfaces;
 
 namespace Victoria {
     /// <summary>
     /// Track information.
     /// </summary>
-    public class LavaTrack : IQueueable {
+    public class LavaTrack {
         /// <summary>
         ///     Track's author.
         /// </summary>
-        public string Author { get; private set; }
+        public string Author { get; }
 
         /// <summary>
         ///     Whether the track is seekable.
         /// </summary>
-        public bool CanSeek { get; private set; }
+        public bool CanSeek { get; }
 
         /// <summary>
         ///     Track's length.
         /// </summary>
-        public TimeSpan Duration { get; private set; }
+        public TimeSpan Duration { get; }
 
         /// <summary>
         ///     Track's encoded hash.
         /// </summary>
-        public string Hash { get; private set; }
+        public string Hash { get; }
 
         /// <summary>
         ///     Audio / Video track Id.
         /// </summary>
-        public string Id { get; private set; }
+        public string Id { get; }
 
         /// <summary>
         ///     Whether the track is a stream.
         /// </summary>
-        public bool IsStream { get; private set; }
+        public bool IsStream { get; }
 
         /// <summary>
         ///     Track's current position.
         /// </summary>
-        public TimeSpan Position { get; private set; }
+        public TimeSpan Position { get; internal set; }
 
         /// <summary>
         ///     Track's title.
         /// </summary>
-        public string Title { get; private set; }
+        public string Title { get; }
 
         /// <summary>
         ///     Track's url.
         /// </summary>
-        public string Url { get; private set; }
+        public string Url { get; }
 
         /// <summary>
         /// 
@@ -98,51 +97,6 @@ namespace Victoria {
             Duration = lavaTrack.Duration;
             CanSeek = lavaTrack.CanSeek;
             IsStream = lavaTrack.IsStream;
-        }
-
-        internal LavaTrack WithHash(string hash) {
-            Hash = hash;
-            return this;
-        }
-
-        internal LavaTrack WithTitle(string title) {
-            Title = title;
-            return this;
-        }
-
-        internal LavaTrack WithAuthor(string author) {
-            Author = author;
-            return this;
-        }
-
-        internal LavaTrack WithDuration(long duration) {
-            Duration = duration < TimeSpan.MaxValue.Ticks
-                ? TimeSpan.FromMilliseconds(duration)
-                : TimeSpan.MaxValue;
-            return this;
-        }
-
-        internal LavaTrack WithId(string id) {
-            Id = id;
-            return this;
-        }
-
-        internal LavaTrack WithStream(bool isStream) {
-            IsStream = isStream;
-            return this;
-        }
-
-        internal LavaTrack WithUrl(string url) {
-            Url = url;
-            return this;
-        }
-
-        internal void WithPosition(TimeSpan position) {
-            Position = position;
-        }
-
-        internal void WithSeek(bool isSeekable) {
-            CanSeek = isSeekable;
         }
     }
 }
