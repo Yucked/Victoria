cc924990ddb6515c1b0575407c39be87c35a4268
Yucked <yucked@pm.me>
2021-05-07 23:43:48 -0400

    RoutePlanner.cs finished

--- src/RoutePlanner.cs
+++ src/RoutePlanner.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -15,7 +14,11 @@ namespace Victoria {
         /// </summary>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        public async Task<RouteStatus> GetStatusAsync(NodeConfiguration nodeConfiguration) {
+        public static async Task<RouteStatus> GetStatusAsync(NodeConfiguration nodeConfiguration) {
+            if (nodeConfiguration == null) {
+                throw new ArgumentNullException(nameof(nodeConfiguration));
+            }
+
             var requestMessage =
                 new HttpRequestMessage(HttpMethod.Get, $"{nodeConfiguration.HttpEndpoint}/routeplanner/status") {
                     Headers = {
@@ -31,26 +34,30 @@ namespace Victoria {
         /// <summary>
         ///     Unmark a failed address.
         /// </summary>
+        /// <param name="nodeConfiguration"></param>
         /// <param name="address"></param>
         /// <returns></returns>
         /// <exception cref="Exception">Throws if Lavalink throws an exception.</exception>
-        public async Task FreeAddressAsync(string address) {
-            var payload = JsonSerializer.SerializeToUtf8Bytes(new {
-                address
-            });
-
-            var responseMessage = await _httpClient
-                .PostAsync("/routeplanner/free/address", new ByteArrayContent(payload))
-                .ConfigureAwait(false);
-
-            if (responseMessage.IsSuccessStatusCode) {
-                return;
+        public static async Task FreeAddressAsync(NodeConfiguration nodeConfiguration, string address) {
+            if (nodeConfiguration == null) {
+                throw new ArgumentNullException(nameof(nodeConfiguration));
             }
 
-            var responseContent = await responseMessage.Content.ReadAsByteArrayAsync()
-                .ConfigureAwait(false);
+            if (string.IsNullOrWhiteSpace(address)) {
+                throw new ArgumentNullException(nameof(address));
+            }
 
-            var routeResponse = JsonSerializer.Deserialize<RouteResponse>(responseContent);
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post,
+                $"{nodeConfiguration.HttpEndpoint}/routeplanner/free/address") {
+                Headers = {
+                    {"Authorization", nodeConfiguration.Authorization}
+                },
+                Content = new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(new {
+                    address
+                }))
+            };
+
+            var routeResponse = await Extensions.ReadAsJsonAsync<RouteResponse>(requestMessage);
             throw new Exception($"{routeResponse.Error} - {routeResponse.Message}");
         }
 
@@ -59,19 +66,19 @@ namespace Victoria {
         /// </summary>
         /// <returns></returns>
         /// <exception cref="Exception">Throws if Lavalink throws an exception.</exception>
-        public async Task FreeAllAsync() {
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/routeplanner/free/all");
-            using var responseMessage = await _httpClient.SendAsync(requestMessage)
-                .ConfigureAwait(false);
-
-            if (responseMessage.StatusCode == HttpStatusCode.NoContent) {
-                return;
+        public static async Task FreeAllAsync(NodeConfiguration nodeConfiguration) {
+            if (nodeConfiguration == null) {
+                throw new ArgumentNullException(nameof(nodeConfiguration));
             }
 
-            var responseContent = await responseMessage.Content.ReadAsByteArrayAsync()
-                .ConfigureAwait(false);
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post,
+                $"{nodeConfiguration.HttpEndpoint}/routeplanner/free/all") {
+                Headers = {
+                    {"Authorization", nodeConfiguration.Authorization}
+                }
+            };
 
-            var routeResponse = JsonSerializer.Deserialize<RouteResponse>(responseContent);
+            var routeResponse = await Extensions.ReadAsJsonAsync<RouteResponse>(requestMessage);
             throw new Exception($"{routeResponse.Error} - {routeResponse.Message}");
         }
     }
