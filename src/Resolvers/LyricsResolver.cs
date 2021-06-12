0ff29b6334d8767a29924bb24bf94f3b078f1658
Yucked <yucked@protonmail.com>
2021-06-12 17:25:49 -0400

    Fixed genius lyrics parser

--- src/Resolvers/LyricsResolver.cs
+++ src/Resolvers/LyricsResolver.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Victoria.Player;
@@ -20,52 +22,71 @@ namespace Victoria.Resolvers {
         private const string EP_GEN = "https://genius.com/{0}-{1}-lyrics";
 
         /// <summary>
-        ///     Searches Genius for lyrics and returns them as string.
+        /// 
         /// </summary>
-        /// <param name="lavaTrack">
-        ///     <see cref="LavaTrack" />
-        /// </param>
-        /// <returns>
-        ///     <see cref="string" />
-        /// </returns>
-        /// <exception cref="ArgumentNullException">Throws if LavaTrack is null.</exception>
-        public static async ValueTask<string> SearchGeniusAsync(LavaTrack lavaTrack) {
-            if (lavaTrack == null) {
-                throw new ArgumentNullException(nameof(lavaTrack));
+        /// <param name="artist"></param>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public static async ValueTask<string> SearchGeniusAsync(string artist, string title) {
+            if (string.IsNullOrWhiteSpace(artist)) {
+                throw new ArgumentNullException(nameof(artist));
             }
 
-            var (author, title) = GetAuthorAndTitle(lavaTrack);
-            var authorTitle = $"{author}{title}"
-                .TrimStart()
-                .TrimEnd()
-                .Replace(' ', '-');
+            if (string.IsNullOrWhiteSpace(title)) {
+                throw new ArgumentNullException(nameof(title));
+            }
 
-            var responseMessage = await Extensions.HttpClient.GetAsync($"https://genius.com/{authorTitle}-lyrics");
+            using var requestMessage =
+                new HttpRequestMessage(HttpMethod.Get, string.Format(EP_GEN, artist, title));
+            var responseMessage = await Extensions.HttpClient.SendAsync(requestMessage);
+
             if (!responseMessage.IsSuccessStatusCode) {
-                throw new Exception(responseMessage.ReasonPhrase);
+                throw new HttpRequestException(responseMessage.ReasonPhrase);
             }
 
             using var content = responseMessage.Content;
             var responseData = await content.ReadAsByteArrayAsync();
 
-            string ParseGeniusHtml() {
-                var start = Encoding.UTF8.GetBytes("<!--sse-->");
-                var end = Encoding.UTF8.GetBytes("<!--/sse-->");
+            string ExtractJson() {
+                var start = Encoding.UTF8.GetBytes("\\\"children\\\":[{\\\"children\\\":[");
+                var end = Encoding.UTF8.GetBytes("\\\"\\\"],\\\"tag\\\":\\\"root\\\"}");
 
                 Span<byte> bytes = responseData;
                 bytes = bytes[bytes.LastIndexOf(start)..];
-                bytes = bytes[..bytes.LastIndexOf(end)];
-
-                var rawHtml = Encoding.UTF8.GetString(bytes);
-                if (rawHtml.Contains("Genius.ads")) {
-                    return string.Empty;
-                }
-
-                var htmlRegex = new Regex("<[^>]*?>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                return htmlRegex.Replace(rawHtml, string.Empty).TrimStart().TrimEnd();
+                bytes = bytes[..(bytes.LastIndexOf(end) + end.Length)];
+                return Encoding.UTF8.GetString(bytes[28..^39])
+                    .Replace("\'", string.Empty)
+                    .Replace("\\", string.Empty);
             }
 
-            return ParseGeniusHtml();
+            var jsonDocument = JsonDocument.Parse(ExtractJson());
+            var lyrics = new Queue<string>();
+
+            void ExtractLyrics(IEnumerable<JsonElement> elements) {
+                foreach (var element in elements) {
+                    switch (element.ValueKind) {
+                        case JsonValueKind.String:
+                            var str = element.GetString();
+                            if (string.IsNullOrWhiteSpace(str)) {
+                                break;
+                            }
+
+                            lyrics.Enqueue(str);
+                            break;
+
+                        case JsonValueKind.Object:
+                            if (!element.TryGetProperty("children", out var child)) {
+                                break;
+                            }
+
+                            ExtractLyrics(child.EnumerateArray());
+                            break;
+                    }
+                }
+            }
+
+            ExtractLyrics(jsonDocument.RootElement.EnumerateArray());
+            return string.Join(Environment.NewLine, lyrics);
         }
 
         /// <summary>
@@ -101,11 +122,15 @@ namespace Victoria.Resolvers {
 
             using var requestMessage =
                 new HttpRequestMessage(HttpMethod.Get, string.Format(EP_OVH, artist, title));
+            var jsonRoot = await Extensions.GetJsonRootAsync(requestMessage, Extensions.DefaultTimeout);
 
-            var jsonRoot = await Extensions.GetJsonRootAsync(requestMessage);
             return !jsonRoot.TryGetProperty("lyrics", out var lyricsElement)
                 ? $"{jsonRoot.GetProperty("error")}"
                 : NewLineRegex.Replace($"{lyricsElement}", "\n");
+        }
+
+        internal static (string Artist, string Title) GetArtistAndTitle(LavaTrack lavaTrack) {
+            return default;
         }
 
         internal static (string Author, string Title) GetAuthorAndTitle(LavaTrack lavaTrack) {
