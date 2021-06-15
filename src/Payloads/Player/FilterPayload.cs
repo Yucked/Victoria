a1ab7972eea2d5a92437d371402fbb451df858c5
Yucked <yucked@protonmail.com>
2021-06-15 13:57:45 -0400

    Ctor for multiple filters

--- src/Payloads/Player/FilterPayload.cs
+++ src/Payloads/Player/FilterPayload.cs
@@ -11,34 +11,49 @@ namespace Victoria.Payloads.Player {
         public IEnumerable<EqualizerBand> Bands { get; }
 
         [JsonPropertyName("karoke")]
-        public KarokeFilter Karoke { get; }
+        public KarokeFilter Karoke { get; set; }
 
         [JsonPropertyName("timescale")]
-        public TimescaleFilter Timescale { get; }
+        public TimescaleFilter Timescale { get; set; }
 
         [JsonPropertyName("tremolo")]
-        public TremoloFilter Tremolo { get; }
+        public TremoloFilter Tremolo { get; set; }
 
         [JsonPropertyName("vibrato")]
-        public VibratoFilter Vibrato { get; }
-        
+        public VibratoFilter Vibrato { get; set; }
+
         [JsonPropertyName("rotation")]
-        public RotationFilter Rotation { get; }
-        
+        public RotationFilter Rotation { get; set; }
+
         [JsonPropertyName("distortion")]
-        public DistortionFilter Distortion { get; }
-        
+        public DistortionFilter Distortion { get; set; }
+
         [JsonPropertyName("channelMix")]
-        public ChannelMixFilter ChannelMix { get; }
-        
+        public ChannelMixFilter ChannelMix { get; set; }
+
         [JsonPropertyName("lowPass")]
-        public LowPassFilter LowPass { get; }
+        public LowPassFilter LowPass { get; set; }
 
         public FilterPayload(ulong guildId, IFilter filter, double volume, IEnumerable<EqualizerBand> bands)
             : base(guildId, "filters") {
             Volume = volume;
             Bands = bands;
+            SetFilter(filter);
+        }
 
+        public FilterPayload(ulong guildId,
+                             IEnumerable<IFilter> filters,
+                             double volume,
+                             IEnumerable<EqualizerBand> bands)
+            : base(guildId, "filters") {
+            Volume = volume;
+            Bands = bands;
+            foreach (var filter in filters) {
+                SetFilter(filter);
+            }
+        }
+
+        private void SetFilter(IFilter filter) {
             switch (filter) {
                 case KarokeFilter karokeFilter:
                     Karoke = karokeFilter;
@@ -51,23 +66,23 @@ namespace Victoria.Payloads.Player {
                 case TremoloFilter tremoloFilter:
                     Tremolo = tremoloFilter;
                     break;
-                
+
                 case VibratoFilter vibratoFilter:
                     Vibrato = vibratoFilter;
                     break;
-                
+
                 case RotationFilter rotationFilter:
                     Rotation = rotationFilter;
                     break;
-                
+
                 case DistortionFilter distortionFilter:
                     Distortion = distortionFilter;
                     break;
-                
+
                 case ChannelMixFilter channelMixFilter:
                     ChannelMix = channelMixFilter;
                     break;
-                
+
                 case LowPassFilter lowPassFilter:
                     LowPass = lowPassFilter;
                     break;
