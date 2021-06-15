777de81b68d1b51d8ff13611a46dced7d68c03a8
Yucked <mail@dvlp.nill.sh>
2021-06-15 15:55:22 -0400

    added SearchType.cs, god I hate v5

---
+++ src/Responses/Rest/SearchType.cs
@@ -0,0 +1,26 @@
+namespace Victoria.Responses.Rest {
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum SearchType {
+        /// <summary>
+        /// 
+        /// </summary>
+        YouTube,
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        YouTubeMusic,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        SoundCloud,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Direct,
+    }
+}
