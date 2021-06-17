69f1bc92a4c79b6838125587eeb28f60936e6f45
Yucked <mail@dvlp.nill.sh>
2021-06-17 19:35:12 -0400

    Updated PlaylistInfo.cs RestException.cs and SearchResponse.cs

--- src/Responses/Rest/PlaylistInfo.cs
+++ src/Responses/Rest/PlaylistInfo.cs
@@ -1,24 +1,20 @@
+using System.Text.Json.Serialization;
+
 namespace Victoria.Responses.Rest {
 	/// <summary>
 	///     Only available if LoadStatus was PlaylistLoaded.
 	/// </summary>
-	public struct PlaylistInfo {
-		/// <summary>
-		///     Playlist name.
-		/// </summary>
-		public string Name { get; private set; }
+    public struct SearchPlaylist {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("name"), JsonInclude]
+        public string Name { get; private set; }
 
-		/// <summary>
-		///     Which track was selected in playlist.
-		/// </summary>
-		public int SelectedTrack { get; private set; }
-
-		internal void WithName(string name) {
-			Name = name;
-		}
-
-		internal void WithTrack(int track) {
-			SelectedTrack = track;
-		}
-	}
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("selectedTrack"), JsonInclude]
+        public int SelectedTrack { get; private set; }
+    }
 }
