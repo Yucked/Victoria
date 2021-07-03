6c123d16cf6105a28f1cf5e36eb7ff4f5a299889
Yucked <mail@dvlp.nill.sh>
2021-07-03 16:24:30 -0400

    Added LongToTimeSpanConverter.cs

---
+++ src/Converters/LongToTimeSpanConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Victoria.Converters {
+    internal sealed class LongToTimeSpanConverter : JsonConverter<TimeSpan> {
+        public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+            var duration = reader.GetInt64();
+            return duration < TimeSpan.MaxValue.Ticks
+                ? TimeSpan.FromMilliseconds(duration)
+                : TimeSpan.MaxValue;
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options) {
+            writer.WriteNumberValue(value.TotalMilliseconds);
+        }
+    }
+}
