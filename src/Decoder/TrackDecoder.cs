7e2d16a1d163525467c44fc27806523ccf1fb439
Yucked <yucked@protonmail.com>
2021-07-03 16:24:22 -0400

    TrackDecoder.cs now contains the Rest endpoint for decoding hashes

--- src/Decoder/TrackDecoder.cs
+++ src/Decoder/TrackDecoder.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Victoria.Decoder {
     /// <summary>
@@ -10,26 +14,26 @@ namespace Victoria.Decoder {
         /// <summary>
         ///     Decodes the hash for the specified track.
         /// </summary>
-        /// <param name="hash">Track's hash.</param>
+        /// <param name="trackHash">Track's hash.</param>
         /// <returns>
         ///     <see cref="LavaTrack" />
         /// </returns>
-        public static LavaTrack Decode(string hash) {
-            Span<byte> hashBuffer = stackalloc byte[hash.Length];
-            Encoding.ASCII.GetBytes(hash, hashBuffer);
+        public static LavaTrack Decode(string trackHash) {
+            Span<byte> hashBuffer = stackalloc byte[trackHash.Length];
+            Encoding.ASCII.GetBytes(trackHash, hashBuffer);
             Base64.DecodeFromUtf8InPlace(hashBuffer, out var bytesWritten);
-            var javaReader = new JavaBinaryReader(hashBuffer.Slice(0, bytesWritten));
+            var javaReader = new JavaBinaryReader(hashBuffer[..bytesWritten]);
 
             // Reading header
             var header = javaReader.Read<int>();
-            var flags = (int) ((header & 0xC0000000L) >> 30);
+            var flags = (int)((header & 0xC0000000L) >> 30);
             var hasVersion = (flags & 1) != 0;
-            var version = hasVersion
+            var _ = hasVersion
                 ? javaReader.Read<sbyte>()
                 : 1;
 
             var track = new LavaTrack(
-                hash,
+                trackHash,
                 title: javaReader.ReadString(),
                 author: javaReader.ReadString(),
                 duration: javaReader.Read<long>(),
@@ -42,6 +46,72 @@ namespace Victoria.Decoder {
                 canSeek: true);
 
             return track;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lavaConfig"></param>
+        /// <param name="trackHash">Track's hash.</param>
+        /// <returns>
+        ///     <see cref="LavaTrack" />
+        /// </returns>
+        public static async Task<LavaTrack> DecodeAsync(LavaConfig lavaConfig, string trackHash) {
+            if (lavaConfig == null) {
+                throw new ArgumentNullException(nameof(lavaConfig));
+            }
+
+            if (string.IsNullOrWhiteSpace(trackHash)) {
+                throw new ArgumentNullException(nameof(trackHash));
+            }
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"{lavaConfig.HttpEndpoint}/decodetrack?track={trackHash}") {
+                Headers = {
+                    { "Authorization", lavaConfig.Authorization }
+                }
+            };
+
+            var lavaTrack = await VictoriaExtensions.ReadAsJsonAsync<LavaTrack>(requestMessage);
+            lavaTrack.Hash = trackHash;
+
+            return lavaTrack;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lavaConfig"></param>
+        /// <param name="trackHashes"></param>
+        /// <returns></returns>
+        public static Task<IEnumerable<LavaTrack>> DecodeAsync(LavaConfig lavaConfig,
+                                                               params string[] trackHashes) {
+            return DecodeAsync(lavaConfig, (IEnumerable<string>)trackHashes);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lavaConfig"></param>
+        /// <param name="trackHashes"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static Task<IEnumerable<LavaTrack>> DecodeAsync(LavaConfig lavaConfig,
+                                                               IEnumerable<string> trackHashes) {
+            if (lavaConfig == null) {
+                throw new ArgumentNullException(nameof(lavaConfig));
+            }
+
+            var requestMessage =
+                new HttpRequestMessage(HttpMethod.Post, $"{lavaConfig.HttpEndpoint}/decodetracks") {
+                    Headers = {
+                        { "Authorization", lavaConfig.Authorization }
+                    },
+                    Content = new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(trackHashes))
+                };
+
+            return VictoriaExtensions
+                .ReadAsJsonAsync<IEnumerable<LavaTrack>>(requestMessage, VictoriaExtensions.LavaTrackConverter);
         }
     }
 }
