507476a493647b015ddd149e5947f7f75108145b
Yucked <mail@dvlp.nill.sh>
2021-07-03 16:24:22 -0400

    TrackDecoder.cs now contains the Rest endpoint for decoding hashes

--- src/Decoder/JavaBinaryReader.cs
+++ src/Decoder/JavaBinaryReader.cs
@@ -3,53 +3,53 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Victoria.Decoder {
-	internal ref struct JavaBinaryReader {
-		private readonly Span<byte> _bytes;
-		private int _position;
+    internal ref struct JavaBinaryReader {
+        private readonly Span<byte> _bytes;
+        private int _position;
 
-		public JavaBinaryReader(Span<byte> bytes) {
-			_bytes = bytes;
-			_position = 0;
-		}
+        public JavaBinaryReader(Span<byte> bytes) {
+            _bytes = bytes;
+            _position = 0;
+        }
 
-		public string ReadString() {
-			var length = Read<short>();
-			var newPosition = _position + length;
+        public string ReadString() {
+            var length = Read<short>();
+            var newPosition = _position + length;
 
-			if (newPosition > _bytes.Length) {
-				throw new Exception("String length exceeds buffer length.");
-			}
+            if (newPosition > _bytes.Length) {
+                throw new Exception("String length exceeds buffer length.");
+            }
 
-			var result = Encoding.UTF8.GetString(_bytes.Slice(_position, length));
-			_position = newPosition;
+            var result = Encoding.UTF8.GetString(_bytes.Slice(_position, length));
+            _position = newPosition;
 
-			return result;
-		}
+            return result;
+        }
 
-		public T Read<T>() where T : struct {
-			T result = default;
-			var bytes = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref result, 1));
-			Read(bytes);
+        public T Read<T>() where T : struct {
+            T result = default;
+            var bytes = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref result, 1));
+            Read(bytes);
 
-			if (BitConverter.IsLittleEndian) {
-				bytes.Reverse();
-			}
+            if (BitConverter.IsLittleEndian) {
+                bytes.Reverse();
+            }
 
-			return result;
-		}
+            return result;
+        }
 
-		private void Read(Span<byte> destination) {
-			var newPosition = _position + destination.Length;
+        private void Read(Span<byte> destination) {
+            var newPosition = _position + destination.Length;
 
-			if (newPosition > _bytes.Length) {
-				throw new Exception("Destination buffer is too large.");
-			}
+            if (newPosition > _bytes.Length) {
+                throw new Exception("Destination buffer is too large.");
+            }
 
-			_bytes
-			   .Slice(_position, destination.Length)
-			   .CopyTo(destination);
+            _bytes
+                .Slice(_position, destination.Length)
+                .CopyTo(destination);
 
-			_position = newPosition;
-		}
-	}
+            _position = newPosition;
+        }
+    }
 }
