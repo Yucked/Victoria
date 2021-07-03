c6121486a3c9737c2ff014a656ed3bbea594a926
Yucked <mail@dvlp.nill.sh>
2021-07-03 16:25:07 -0400

    meh nothing major here

--- src/Converters/LoadStatusConverter.cs
+++ src/Converters/LoadStatusConverter.cs
@@ -2,11 +2,12 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Victoria.Enums;
+using Victoria.Responses.Search;
 
 namespace Victoria.Converters {
     internal sealed class LoadStatusConverter : JsonConverter<LoadStatus> {
         public override LoadStatus Read(ref Utf8JsonReader reader, Type typeToConvert,
-                                          JsonSerializerOptions options) {
+                                        JsonSerializerOptions options) {
             return (LoadStatus)reader.ValueSpan[0];
         }
 
