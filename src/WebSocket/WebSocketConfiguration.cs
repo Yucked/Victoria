ac3d9cac0fe1a5ed7613d1c3c9622e79e042bf35
Yucked <yucked@protonmail.com>
2021-07-05 14:02:07 -0400

    Added WebSocketConfiguration.cs

---
+++ src/WebSocket/WebSocketConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Victoria.WebSocket {
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class WebSocketConfiguration {
+        /// <summary>
+        ///     Max buffer size for receiving websocket message.
+        /// </summary>
+        public ushort BufferSize { get; set; }
+            = 512;
+
+        /// <summary>
+        ///     How many reconnect attempts are allowed.
+        /// </summary>
+        public int ReconnectAttempts { get; set; }
+            = 10;
+
+        /// <summary>
+        ///     Reconnection delay for retrying websocket connection.
+        /// </summary>
+        public TimeSpan ReconnectDelay { get; set; }
+            = TimeSpan.FromSeconds(10);
+
+        internal string Endpoint { get; set; }
+    }
+}
