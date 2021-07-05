aaa27faf0e7d88c552519db6c9f05a9a067d9d38
Yucked <yucked@protonmail.com>
2021-07-05 14:04:02 -0400

    Added RetryEventArgs.cs

---
+++ src/WebSocket/EventArgs/RetryEventArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace Victoria.WebSocket.EventArgs {
+    /// <summary>
+    /// 
+    /// </summary>
+    public readonly struct RetryEventArgs {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Count { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsLastRetry { get; }
+
+        internal RetryEventArgs(int count, bool isLastRetry) {
+            Count = count;
+            IsLastRetry = isLastRetry;
+        }
+    }
+}
