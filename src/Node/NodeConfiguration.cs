c5fedca4f27d8ab9b0f52e134a5e4ffbbd8f303c
Yucked <yucked@protonmail.com>
2021-07-05 14:03:22 -0400

    NodeConfiguration contains WebSocketConfiguration now instead of indv props

--- src/Node/NodeConfiguration.cs
+++ src/Node/NodeConfiguration.cs
@@ -1,4 +1,5 @@
 using System;
+using Victoria.WebSocket;
 
 namespace Victoria.Node {
     /// <summary>
@@ -6,64 +7,65 @@ namespace Victoria.Node {
     /// </summary>
     public sealed class NodeConfiguration {
         /// <summary>
-        ///     Max buffer size for receiving websocket message.
         /// </summary>
-        public ushort BufferSize { get; set; } = 512;
-
-        /// <summary>
-        /// </summary>
-        public bool EnableResume { get; set; } = false;
+        public bool EnableResume { get; set; }
+            = false;
 
         /// <summary>
         ///     Server's IP/Hostname.
         /// </summary>
-        public string Hostname { get; set; } = "127.0.0.1";
+        public string Hostname { get; set; }
+            = "127.0.0.1";
 
         /// <summary>
         ///     Port to connect to.
         /// </summary>
-        public ushort Port { get; set; } = 2333;
+        public ushort Port { get; set; }
+            = 2333;
 
         /// <summary>
         ///     Server's password/authentication.
         /// </summary>
-        public string Authorization { get; set; } = "youshallnotpass";
+        public string Authorization { get; set; }
+            = "youshallnotpass";
 
         /// <summary>
         ///     Use Secure Socket Layer (SSL) security protocol when connecting to Lavalink.
         /// </summary>
-        public bool IsSecure { get; init; } = false;
+        public bool IsSecure { get; init; }
+            = false;
 
         /// <summary>
         ///     Applies User-Agent header to all requests.
         /// </summary>
-        public string UserAgent { get; set; } = null;
-
-        /// <summary>
-        ///     How many reconnect attempts are allowed.
-        /// </summary>
-        public int ReconnectAttempts { get; set; } = 10;
-
-        /// <summary>
-        ///     Reconnection delay for retrying websocket connection.
-        /// </summary>
-        public TimeSpan ReconnectDelay { get; set; } = TimeSpan.FromSeconds(10);
+        public string UserAgent { get; set; }
+            = null;
 
         /// <summary>
         /// </summary>
-        public string ResumeKey { get; set; } = nameof(Victoria);
+        public string ResumeKey { get; set; }
+            = nameof(Victoria);
 
         /// <summary>
         /// </summary>
-        public TimeSpan ResumeTimeout { get; set; } = TimeSpan.FromSeconds(30);
+        public TimeSpan ResumeTimeout { get; set; }
+            = TimeSpan.FromSeconds(30);
 
         /// <summary>
         ///     Whether to enable self deaf for bot.
         /// </summary>
-        public bool SelfDeaf { get; set; } = true;
+        public bool SelfDeaf { get; set; }
+            = true;
 
-        internal string SocketEndpoint
-            => (IsSecure ? "wss" : "ws") + Endpoint;
+        /// <summary>
+        /// 
+        /// </summary>
+        public WebSocketConfiguration SocketConfiguration { get; set; }
+            = new() {
+                ReconnectAttempts = 10,
+                ReconnectDelay = TimeSpan.FromSeconds(3),
+                BufferSize = 512
+            };
 
         internal string HttpEndpoint
             => (IsSecure ? "https" : "http") + Endpoint;
