e8e5fdcd228a21ef8e2fbf7f7cb4a3f3fd2cb010
Yucked <yucked@protonmail.com>
2021-07-14 18:57:41 -0400

    Moved EqualizerBand.cs from Payloads to Filters

--- src/Filters/EqualizerBand.cs
+++ src/Filters/EqualizerBand.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Serialization;
 
 #pragma warning disable 8632
 
-namespace Victoria.Payloads.Player {
+namespace Victoria.Filters {
     /// <summary>
     ///     Equalizer band
     /// </summary>
