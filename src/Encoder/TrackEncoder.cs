8e21eff7bb05186a72e775c53404d7c4dcb325cb
RofLBotZ94 <45794321+RofLBotZ94@users.noreply.github.com>
2022-08-06 17:44:47 +0100

    Error checks for JavaBinaryWriter and tweaks to the TrackEncoder

--- src/Encoder/TrackEncoder.cs
+++ src/Encoder/TrackEncoder.cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace Victoria.Encoder {
     /// <summary>
-    /// Helper class for decoding Lavalink's <see cref="LavaTrack"/> hash.
+    /// Helper class for encoding Lavalink's <see cref="LavaTrack"/>.
     /// </summary>
     public readonly struct TrackEncoder {
         private const int TRACK_VERSIONED = 1;
         private const int TRACK_VERSION = 2;
 
         /// <summary>
-        ///     Decodes the hash for the specified track.
+        ///     Encodes the specified track and sets its hash.
         /// </summary>
         /// <param name="track">The <see cref="LavaTrack"/> to encode.</param>
         /// <remarks>
-        /// This method sets the track's hash in place.
+        ///     This method sets the track's hash in place.
         /// </remarks>
         public static void Encode(LavaTrack track) {
-            Span<byte> bytes = stackalloc byte[GetByteCount(track)];
+            Span<byte> bytes = stackalloc byte[GetMaxByteCount(track)];
 
             var javaWriter = new JavaBinaryWriter(bytes);
             javaWriter.Write<byte>(TRACK_VERSION);
@@ -27,15 +27,15 @@ namespace Victoria.Encoder {
             javaWriter.Write((long)track.Duration.TotalMilliseconds);
             javaWriter.Write(track.Id);
             javaWriter.Write(track.IsStream);
-            javaWriter.WriteNullableText(track.Url); // Extension method
+            javaWriter.WriteNullableText(track.Url);
             javaWriter.Write(track.Source);
             javaWriter.Write((long)track.Position.TotalMilliseconds);
-            javaWriter.WriteVersioned(TRACK_VERSIONED); // Extension method
+            javaWriter.WriteVersioned(TRACK_VERSIONED);
 
-            track.Hash = Convert.ToBase64String(bytes);
+            track.Hash = Convert.ToBase64String(bytes[..javaWriter.Length]);
         }
 
-        private static int GetByteCount(LavaTrack track) {
+        private static int GetMaxByteCount(LavaTrack track) {
             return 23 + typeof(LavaTrack).GetProperties()
                 .Where(p => p.PropertyType == typeof(string) && p.Name != "Hash")
                 .Sum(p => 2 + Encoding.UTF8.GetByteCount(p.GetValue(track).ToString()));
