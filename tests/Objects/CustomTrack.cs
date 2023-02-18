6cf0af844636fe6ed062d7445fc8312181c795e9
Yucked <yucked@pm.me>
2023-02-18 16:24:37 -0500

    BEGONE OLD CODE

--- tests/Objects/CustomTrack.cs
+++
@@ -1,9 +0,0 @@
-using Victoria.Interfaces;
-
-namespace Victoria.Tests.Objects {
-    public sealed class CustomTrack : AbstractLavaTrack {
-        public ulong Requester { get; init; }
-
-        public CustomTrack(ILavaTrack lavaTrack) : base(lavaTrack) { }
-    }
-}
