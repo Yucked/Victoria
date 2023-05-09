6cc72a684e2ff761d7c0312ea795407bcde29120
Yucked <yucked@pm.me>
2023-05-08 21:27:41 -0400

    summary for UpdateSessionRequest.cs

--- src/Rest/Requests/UpdateSessionRequest.cs
+++ src/Rest/Requests/UpdateSessionRequest.cs
@@ -2,6 +2,11 @@ using System.Text.Json.Serialization;
 
 namespace Victoria.Rest.Requests;
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="ShouldResume"></param>
+/// <param name="Timeout"></param>
 public readonly record struct UpdateSessionRequest(
     [property: JsonPropertyName("resuming")]
     bool ShouldResume,
