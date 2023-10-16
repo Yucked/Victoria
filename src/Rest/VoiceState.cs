38359e5062227c1f16b458c649e40cdda601ea69
Yucked <mail@dvlp.nill.sh>
2023-10-16 16:23:58 -0400

    IsConnected and Ping no longer included

--- src/Rest/VoiceState.cs
+++ src/Rest/VoiceState.cs
@@ -8,16 +8,10 @@ namespace Victoria.Rest;
 /// <param name="Token">Discord voice token to authenticate with</param>
 /// <param name="Endpoint">Discord voice endpoint to connect to</param>
 /// <param name="SessionId">Discord voice session id to authenticate with</param>
-/// <param name="IsConnected">Whether the player is connected</param>
-/// <param name="Ping">Roundtrip latency in milliseconds to the voice gateway (-1 if not connected).</param>
 public readonly record struct VoiceState(
     [property: JsonPropertyName("token")]
     string Token,
     [property: JsonPropertyName("endpoint")]
     string Endpoint,
     [property: JsonPropertyName("sessionId")]
-    string SessionId,
-    [property: JsonPropertyName("connected")]
-    bool IsConnected,
-    [property: JsonPropertyName("ping")]
-    int Ping);
+    string SessionId);
