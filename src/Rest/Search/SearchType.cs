1444e6352db14dcac77bb8114ccaa278bdce4b7d
Yucked <yucked@pm.me>
2023-10-16 14:01:42 -0400

    removed old searchtype, renamed searchstatus to searchtype

--- src/Rest/Search/SearchType.cs
+++ src/Rest/Search/SearchType.cs
@@ -1,21 +1,38 @@
-﻿namespace Victoria.Rest.Search {
+﻿using System.Runtime.Serialization;
+
+namespace Victoria.Rest.Search {
     /// <summary>
-    /// 
+    /// Search status when searching for songs via Lavalink.
     /// </summary>
     public enum SearchType {
         /// <summary>
-        /// 
+        /// A track has been loaded
         /// </summary>
-        YouTube,
+        [EnumMember(Value = "track")]
+        Track,
 
         /// <summary>
-        /// 
+        /// A playlist has been loaded
         /// </summary>
-        SoundCloud,
+        [EnumMember(Value = "playlist")]
+        Playlist,
 
         /// <summary>
-        /// 
+        /// A search result has been loaded
         /// </summary>
-        Direct,
+        [EnumMember(Value = "search")]
+        Search,
+
+        /// <summary>
+        /// There has been no matches for your identifier
+        /// </summary>
+        [EnumMember(Value = "empty")]
+        Empty,
+
+        /// <summary>
+        /// Loading has failed with an error
+        /// </summary>
+        [EnumMember(Value = "error")]
+        Error
     }
 }
