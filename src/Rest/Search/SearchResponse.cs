c1a3d107ce3d5d8f3f39cf86acc6d31270338b20
Yucked <yucked@pm.me>
2023-10-16 15:46:48 -0400

    SearchResponse.cs working as expected

--- src/Rest/Search/SearchResponse.cs
+++ src/Rest/Search/SearchResponse.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 // ReSharper disable UnusedAutoPropertyAccessor.Local
@@ -6,29 +8,53 @@ namespace Victoria.Rest.Search {
     /// <summary>
     /// 
     /// </summary>
-    public struct SearchResponse {
+    public sealed class SearchResponse {
         /// <summary>
         /// 
         /// </summary>
-        [JsonPropertyName("loadType"), JsonConverter(typeof(JsonStringEnumConverter))]
-        public SearchStatus Status { get; }
+        [JsonPropertyName("loadType"), JsonConverter(typeof(JsonStringEnumConverter)), JsonInclude]
+        public SearchType Type { get; internal set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [JsonPropertyName("playlistInfo")]
-        public SearchPlaylist Playlist { get; }
+        [JsonPropertyName("playlistInfo"), JsonInclude]
+        public SearchPlaylist Playlist { get; internal set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [JsonPropertyName("exception")]
-        public SearchException Exception { get; }
+        [JsonPropertyName("exception"), JsonInclude]
+        public SearchException Exception { get; internal set; }
 
         /// <summary>
         /// 
         /// </summary>
-        [JsonPropertyName("tracks")]
-        public IReadOnlyCollection<object> Tracks { get; }
+        public IReadOnlyCollection<LavaTrack> Tracks { get; internal set; }
+
+        internal SearchResponse(JsonDocument document) {
+            Type = document.RootElement.GetProperty("loadType").AsEnum<SearchType>();
+            if (document.RootElement.TryGetProperty("playlistInfo", out var playlistElement)) {
+                Playlist = playlistElement.Deserialize<SearchPlaylist>();
+            }
+
+            if (document.RootElement.TryGetProperty("exception", out var exceptionElement)) {
+                Exception = exceptionElement.Deserialize<SearchException>();
+            }
+
+            if (!document.RootElement.TryGetProperty("data", out var dataElement)) {
+                return;
+            }
+
+            Tracks = dataElement.EnumerateArray()
+                .AsParallel()
+                .Select(x => {
+                    var track = x.GetProperty("info").Deserialize<LavaTrack>();
+                    track.Hash = x.GetProperty("encoded").GetString();
+                    track.PluginInfo = x.GetProperty("pluginInfo");
+                    return track;
+                })
+                .ToList();
+        }
     }
 }
