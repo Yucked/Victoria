3a0428bfa0cc5b83a6a2b92a1e8360d604f0a0e4
Yucked <mail@dvlp.nill.sh>
2023-12-01 16:46:59 -0500

    converters and state property

--- src/LavaPlayer.cs
+++ src/LavaPlayer.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Victoria.Converters;
 using Victoria.Rest;
 using Victoria.Rest.Filters;
 
@@ -20,8 +21,8 @@ public class LavaPlayer<TLavaTrack>
     /// 
     /// </summary>
     /// TODO: API doesn't return it this way
-    //[JsonPropertyName("guildId"), JsonInclude]
-    public LavaTrack Track { get; }
+    [JsonPropertyName("track"), JsonInclude, JsonConverter(typeof(LavaTrackConverter))]
+    public LavaTrack Track { get; internal init; }
 
     /// <summary>
     /// 
@@ -45,14 +46,17 @@ public class LavaPlayer<TLavaTrack>
     /// 
     /// </summary>
     [JsonPropertyName("voice"), JsonInclude]
-    public VoiceState VoiceState { get; }
+    public VoiceState VoiceState { get; internal init; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonPropertyName("state"), JsonInclude]
+    public PlayerState State { get; internal init; }
 
     /// <summary>
     /// 
     /// </summary>
     /// TODO: How to handle queue?
     public LavaQueue<TLavaTrack> Queue { get; }
-    
-    // TODO: Need property for player state
-    // https://github.com/lavalink-devs/Lavalink/blob/master/IMPLEMENTATION.md#player-state
 }
